eda35ea9ee45694e07e5faab59e18749c478f1a4
Alex Gorelick <agorelick@mgh.harvard.edu>
2023-03-30 16:10:07 -0400

    adding Rd man files, DESCRIPTION and NAMESPACE

--- main.R
+++ main.R
@@ -17,6 +17,10 @@
 ### 7. adjust all relevant parameters (including a "nnormals") above the line with ########
 
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# add to wiki for installation
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 ## install any missing R CRAN packages:
 required.R.packages <- c('here','gplots','RColorBrewer','colorspace')
 new.R.packages <- required.R.packages[!(required.R.packages %in% installed.packages()[,"Package"])]
@@ -34,17 +38,21 @@ if(length(new.BC.packages)>0) {
 required.packages <- c(required.R.packages, required.BC.packages)
 for(p in required.packages) library(package=p, character.only=T)
 
-## load functions
-source(here('polyG_pipeline/func.R'))
-source(here('polyG_pipeline/subject.R'))
-source(here('polyG_pipeline/angular_distance.R'))
+source(here::here('polyG/R/func.R'))
+source(here::here('polyG/R/angular_distance.R'))
 
+#source(here('polyG_pipeline/subject.R'))
+#source(here('polyG_pipeline/angular_distance.R'))
+
+
+
+library(polyG)
 
 # the folder with the input data for this patient cohort
-input_dir <- here("original_data/polyG_lung")
+input_dir <- '~/lab_repos/crc_lung_met/original_data/polyG_lung'
 
 # where the output "results" will go
-results_dir <- here('processed_data/polyG_lung')
+results_dir <- '~/lab_repos/crc_lung_met/processed_data/polyG_lung'
 
 # the maximum JS distance allowed between replicates
 max_replicate_th <- 0.11
@@ -56,48 +64,7 @@ sample_name_change <- TRUE
 bootstrap <- F
 bscut <- 50 ## not currrently implemented
 
-# load the data_info file for this patient
-data_info <- read.table(file.path(input_dir,"data_info.txt"),sep="\t",header=TRUE,stringsAsFactors=FALSE)
-
-
-#####################
-# for each patient
-for (prow in 1:nrow(data_info)) {
-    set.seed(42) ## reset seed for each patient to enable rerunning for single patients at a time
- 
-    subject_name <- gsub('-Data','',data_info$data_path[prow])
-    message('Running for ',subject_name)
-
-    ## get path, exclusion threshold and selected normal for this patient
-    data_path <- data_info$data_path[prow]
-    sample_exclusion_th <- data_info$sample_exclusion_th[prow]
-    sel_normal_sample <- data_info$sel_normal_sample[prow]
-
-    ## get all the normal samples for this patient
-    all_normal_samples <- as.character(data_info[prow,grep('normal_sample',names(data_info))])
-    all_normal_samples <- all_normal_samples[!is.na(all_normal_samples)]
-
-    ## get all excluded samples for this patient
-    all_excluded_samples <- as.character(data_info[prow,grep('samples_to_exclude',names(data_info))])
-    all_excluded_samples <- all_excluded_samples[!is.na(all_excluded_samples)]
-  
-    ## map old/new names 
-    new_sample_names <- read.table(file.path(input_dir,"AnnotationFiles",paste0(subject_name,"_annotationFile.txt")),sep="\t",header=TRUE,stringsAsFactors=FALSE)
-
-    # create output directory
-    allout_dir <- file.path(results_dir,"results",paste("sample_exclusion",sample_exclusion_th,"rep_cut",max_replicate_th,sep="_"))
-    if (!dir.exists(allout_dir)) dir.create(allout_dir,recursive=TRUE,showWarnings=TRUE )
-
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # select representative replicates and create distance matrices
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-    subject(input_dir, allout_dir, subject_name, data_path, sample_exclusion_th, sel_normal_sample, all_normal_samples, new_sample_names) 
-    
-    ## get angular distance trees
-    angular_distance(input_dir, allout_dir, subject_name, sel_normal_sample, all_normal_samples, new_sample_names, bootstrap, bscut)
-}
-
+polyG(input_dir, results_dir, max_replicate_th, sample_name_change, bootstrap, bscut, seed=42)
 
 
 
